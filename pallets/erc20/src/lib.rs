a4d330578c56f43279b3291993f115b1227d5703
saiifbaba <saifbaba888@gmail.com>
2024-10-29 18:40:36 +0530

    Declared Storage for Token Balances and Metadata

---
+++ pallets/erc20/src/lib.rs
@@ -0,0 +1,16 @@
+
+use frame_support::{
+    decl_module, decl_storage, decl_event, decl_error, dispatch, ensure,
+};
+use frame_system::ensure_signed;
+pub trait Config: frame_system::Config {
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+}
+decl_storage! {
+    trait Store for Module<T: Config> as ERC20 {
+        // Total supply of tokens.
+        TotalSupply get(fn total_supply): u64;
+        // Balances for each account.
+        Balances get(fn balance_of): map hasher(blake2_128_concat) T::AccountId => u64;
+    }
+}
